bee709711fb5e3c9ba831a734e2801351f38a4d1
yuankun.gu <yuankun.gu@yintech.cn>
2022-04-28 20:15:00 +0800

    feat: 配置修改

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+const env = process.env.NEXT_PUBLIC_ENV || 'dev'
+console.log('----env', env)
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['tva1.sinaimg.cn']
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import '@/styles/variables.module.scss';` // 引入全局scss变量
+  }
 }
 
 module.exports = nextConfig
