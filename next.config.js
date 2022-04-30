f73f9a812020bebdfbfe68bd6c83b14281607d36
yuankun.gu <yuankun.gu@yintech.cn>
2022-04-30 19:56:29 +0800

    feat: 增加配置

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const path = require('path')
 const env = process.env.NEXT_PUBLIC_ENV || 'dev'
 console.log('----env', env)
+const config = {
+  dev: {
+    baseDomain: 'http://127.0.0.1:7001',
+  },
+  test: {
+    baseDomain: 'http://1.14.77.152/api',
+  }
+}[env]
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -10,6 +18,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import '@/styles/variables.module.scss';` // 引入全局scss变量
+  },
+  env: {
+    BASE_DOMAIN: config.baseDomain
   }
 }
 
